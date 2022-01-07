c15164042cf173a9ef67dfaca35d012556423c8c
Shumyk <dima.shumyk@gmail.com>
2022-01-07 19:30:18 +0200

    added modal and backdrop components, added it on app.js, now it visible all time

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Backdrop from "./components/Backdrop";
+import Modal from "./components/Modal";
 import Todo from "./components/Todo";
 
 function App() {
@@ -6,6 +8,8 @@ function App() {
     <Todo title='Learn React' />
     <Todo title='Master React' />
     <Todo title='Exlore the full React course' />
+    <Modal />
+    <Backdrop />
   </div>;
 }
 
