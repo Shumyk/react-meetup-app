f825b524dc8761cb7211ad6f1f0bf94292e3307f
Shumyk <dima.shumyk@gmail.com>
2022-01-08 12:40:33 +0200

    added code to NewMeetup.js responsible for sending POST request to DB and saves it. created Firebase DB for developing purpose and successfuly saved first entry

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,7 +2,16 @@ import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
   function onAddMeetupSave(meetupData) {
-
+    fetch(
+      "https://react-meetup-app-a62f6-default-rtdb.europe-west1.firebasedatabase.app/meetups.json",
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    );
   }
 
   return (
